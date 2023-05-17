7e13b69ed94d12abb73a528c330474756bfeb9c2
Kushwaha <kushwaha.priyanshu.imp@gmail.com>
2023-05-18 00:27:34 +0530

    Added speak function using pyttsx3

---
+++ Body/speak.py
@@ -0,0 +1,15 @@
+import pyttsx3     
+
+def speak(text):
+    engine = pyttsx3.init("sapi5")
+    voices = engine.setProperty("rate", 180)
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[1].id)
+    print("")
+    print(f"You : {text}")
+    print("")
+    engine.say(text)
+    engine.runAndWait()
+
+
+
